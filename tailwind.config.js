aff55ce75c6ca2c5ad0c9f4bf40939fe5fd3edc6
Rohith J <146927483+Rohithjeevanantham@users.noreply.github.com>
2025-01-02 10:32:08 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          900: '#1a365d'
+        }
+      }
+    }
+  },
+  plugins: []
+}
